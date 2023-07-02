57855a9c6b4891c975a3a628e4ec9a739b2dc01c
sansmoraxz <31374920+sansmoraxz@users.noreply.github.com>
2023-07-03 02:46:56 +0530

    Server port default set to 8000 as per docs

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 // from env
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
